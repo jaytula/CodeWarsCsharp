b893265bf16630b8f3485cd973b67591fab6036d
Jay T <jaytula@gmail.com>
2021-08-01 01:40:36 -0700

    Initial commit

---
+++ DisariumNumber/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static string DisariumNumber(int number)
+    {
+        var sumOfPowers = number.ToString()
+            .Select((ch, idx) => (int)Math.Pow(int.Parse(ch.ToString()), idx + 1))
+            .Aggregate(0, (acc, curr) => acc + curr);
+        return sumOfPowers == number ? "Disarium !!" : "Not !!";
+    }
+}
