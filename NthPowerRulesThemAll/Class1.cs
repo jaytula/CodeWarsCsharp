b67c3308d28b4ee9e69c9a864fe20690f2b3844c
Jay T <jaytula@gmail.com>
2021-08-01 19:41:10 -0700

    Finish 'Nth power rules them all'

---
+++ NthPowerRulesThemAll/Class1.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Linq;
+
+public class Kata
+{
+    public static int ModifiedSum(int[] a, int n)
+    {
+        return a.Select(val => (int)Math.Pow(val, n)).Sum() - a.Sum();
+    }
+}
---
+++ NthPowerRulesThemAllTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+namespace Solution
+{
+    using NUnit.Framework;
+    using System;
+
+    [TestFixture]
+    public class SolutionTest
+    {
+
+        private static object[] Basic_Test_Cases = new object[]
+        {
+      new object[] {new int[] {1, 2, 3}, 3, 30},
+      new object[] {new int[] {1, 2}, 5, 30},
+        };
+
+        [Test, TestCaseSource(typeof(SolutionTest), "Basic_Test_Cases")]
+        public void Basic_Test(int[] array, int exponent, int expected)
+        {
+            Assert.AreEqual(expected, Kata.ModifiedSum(array, exponent), "Solution should work for the examples provided in the Description");
+        }
+
+        // Note: Random tests output total user code execution time.
+    }
+}
