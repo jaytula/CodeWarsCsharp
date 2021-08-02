a163a37ea45d30168222137ba00cd8f423262032
Jay T <jaytula@gmail.com>
2021-08-01 20:21:36 -0700

    Finish 'Row Weights'

---
+++ RowWeights/Class1.cs
@@ -0,0 +1,16 @@
+﻿public class Kata
+{
+    public static int[] RowWeights(int[] a)
+    {
+        int teamOneWeight = 0;
+        int teamTwoWeight = 0;
+
+        for (int i = 0; i < a.Length; i++)
+        {
+            if (i % 2 == 0) teamOneWeight += a[i];
+            else teamTwoWeight += a[i];
+        }
+
+        return new int[] { teamOneWeight, teamTwoWeight };
+    }
+}
