1637bd249f6e0abebee1b750149354702583da12
Jay T <jaytula@gmail.com>
2021-08-05 16:46:54 -0700

    Finish 'Minimize Sum Of Array (Array Series #1)'

---
+++ MinimizeSumOfArray/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Kata
+{
+    public static int MinSum(int[] a)
+    {
+        int sum = 0;
+
+        Array.Sort(a);
+
+        for (int i = 0; i < a.Length / 2; i++)
+        {
+            sum += a[i] * a[a.Length - 1 - i];
+        }
+
+        return sum;
+    }
+}
