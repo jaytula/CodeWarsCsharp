75595c7f209840512674c7614d29d947d1e33300
Jay T <jaytula@gmail.com>
2021-08-06 16:10:37 -0700

    Finish 'Automorphic Number'

---
+++ AutomorphicNumber/Class1.cs
@@ -0,0 +1,10 @@
+﻿public class Kata
+{
+    public static string Automorphic(int n)
+    {
+        string numberAsString = n.ToString();
+        string squareAsString = (n * n).ToString();
+
+        return numberAsString == squareAsString.Substring(squareAsString.Length - numberAsString.Length) ? "Automorphic" : "Not!!";
+    }
+}
