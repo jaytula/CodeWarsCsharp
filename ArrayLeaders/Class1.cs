9ab82aaf485a59a69e6bc24c2d2c04804dca46ea
Jay T <jaytula@gmail.com>
2021-08-08 18:59:10 -0700

    Finish 'Array Leaders (Array Series #3)'

---
+++ ArrayLeaders/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+public static class Kata
+{
+    public static int[] ArrayLeaders(int[] numbers)
+    {
+        List<int> result = new List<int> { };
+
+        for(int i = 0; i < numbers.Length; i++)
+        {
+            if (numbers[i] > numbers.Skip(i + 1).Sum()) result.Add(numbers[i]);
+        }
+        return result.ToArray();
+    }
+}
